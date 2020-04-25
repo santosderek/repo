d5582e372d8c89b572f1a87c39f206041a0b1977
Derek Santos <derek@dyrenex.com>
2020-04-25 02:34:11 +0000

    Need to specify python2 and check if installed

--- bin/getdotfiles.sh
+++ bin/getdotfiles.sh
@@ -10,16 +10,20 @@ if [ ! -d $CONFIG_DIR ]; then
     mkdir -p $CONFIG_DIR 
 fi
 
+if [ $(dpkg-query -W --showformat='${Status}\n' python2 | grep "install ok installed") != "" ]; then
+    apt install python2
+fi
+
 echo "${GREEN}Getting repo contents...${NOCOLOR}"
 JSON=$(curl -k "$CONFIG_REPO_URL")
 
-COUNT=$(echo $JSON | python -c 'import sys,json;data=json.loads(sys.stdin.read()); print len(data)')
+COUNT=$(echo $JSON | python2 -c 'import sys,json;data=json.loads(sys.stdin.read()); print len(data)')
 
 echo "\n${GREEN}Getting files from repo...${NOCOLOR}"
 for ((POS=0; POS < $COUNT ; POS++))
 do
-    NAME=$(echo $JSON | python -c "import sys,json;data=json.loads(sys.stdin.read()); print data[$POS]['name']")
-    URL=$(echo $JSON | python -c "import sys,json;data=json.loads(sys.stdin.read()); print data[$POS]['download_url']")
+    NAME=$(echo $JSON | python2 -c "import sys,json;data=json.loads(sys.stdin.read()); print data[$POS]['name']")
+    URL=$(echo $JSON | python2 -c "import sys,json;data=json.loads(sys.stdin.read()); print data[$POS]['download_url']")
     
     echo "\nDownloading $NAME from $URL to $CONFIG_DIR/$NAME"
     
